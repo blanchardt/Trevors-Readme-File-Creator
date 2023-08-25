aa5a85a8b720e1ee588e32cb23b141568cda5f4f
unknown <blancardt@merrimack.edu>
2023-08-25 14:07:00 -0400

    added the prompt for asking for install dependencies and added a installation section to the README text.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Trevors-Readme-File-Creator
 Modify the starter code to create a command line application that creates a README.md file based off users inputs.
-![License: MIT](https://img.shields.io/badge/license-MIT-blue.svg)
--- index.js
+++ index.js
@@ -2,17 +2,24 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const questions = ['What is your GitHub username?', 'What is your email address?', 'What is your project\'s name?', 'Please writ a short description of your project?', 'What kind of license does your project have?', 'What command should be run to install dependencies?', 'What command should be run to run tests?', 'What does the user need to know about using the repo?', 'What does the user need to know about contributing to the repo?'];
+const questions = ['What is your GitHub username?', 'What is your email address?', 'What is your project\'s name?', 'Please write a short description of your project?', 'What kind of license does your project have?', 'What command should be run to install dependencies?', 'What command should be run to run tests?', 'What does the user need to know about using the repo?', 'What does the user need to know about contributing to the repo?'];
 
 //Went to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll to figure out how to replace spaces with underscores.
 //also credited in the README file.
 //function that creates the text for the README file.
-const createText = ({ userName, email, title, description, license }) =>
+const createText = ({ userName, email, title, description, license, install }) =>
   `# ${title}  
 ![License: ${license}](https://img.shields.io/badge/License-${license.replaceAll(' ', '_')}-blue.svg)    
 
 ## Description  
 ${description}  
+
+## Installation  
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${install}
+\`\`\`
 
 ## Questions  
 If you have any questions about the repo, open an issue or contact me directly at ${email}.  You can find more of my work at [${userName}](https://github.com/${userName}/).  `;
@@ -54,6 +61,12 @@ function init() {
       message: questions[4],
       choices: [ "MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None" ],
       name: 'license',
+    },
+    {
+      type: 'input',
+      message: questions[5],
+      default: 'npm i',
+      name: 'install',
     },
   ])
   .then((data) => {
