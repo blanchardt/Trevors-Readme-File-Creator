32ea5dc9770927070bccdd05d2c9f78365f743a3
unknown <blancardt@merrimack.edu>
2023-08-25 10:43:45 -0400

    added the propmt for asking the projects name and set the users response as the first header in the README file.

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const fs = require('fs');
 const questions = ['What is your GitHub username?', 'What is your email address?', 'What is your project\'s name?', 'Please writ a short description of your project?', 'What kind of license should your project have?', 'What command should be run to install dependencies?', 'What command should be run to run tests?', 'What does the user need to know about using the repo?', 'What does the user need to know about contributing to the repo?'];
 
 //function that creates the text for the README file.
-const createText = ({ userName, email }) =>
-  `## Questions
-If you have any questions about the repo, open an issue or contact me directly at ${email}.  You can find more of my work at [${userName}](https://github.com/${userName}/).`;
+const createText = ({ userName, email, title }) =>
+  `# ${title}  
+## Questions  
+If you have any questions about the repo, open an issue or contact me directly at ${email}.  You can find more of my work at [${userName}](https://github.com/${userName}/).  `;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -30,6 +31,11 @@ function init() {
       type: 'input',
       message: questions[1],
       name: 'email',
+    },
+    {
+      type: 'input',
+      message: questions[2],
+      name: 'title',
     },
   ])
   .then((data) => {
