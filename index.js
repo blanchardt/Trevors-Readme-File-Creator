4761b7f59851ca91fec8c33fd639c6e3dda45d97
unknown <blancardt@merrimack.edu>
2023-08-25 09:39:59 -0400

    call the write to function in the init function and created a folder location for the new README.md file.

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const fs = require('fs');
 const questions = ['What is your GitHub username?', 'What is your email address?', 'What is your project\'s name?', 'Please writ a short description of your project?', 'What kind of license should your project have?', 'What command should be run to install dependencies?', 'What command should be run to run tests?', 'What does the user need to know about using the repo?', 'What does the user need to know about contributing to the repo?'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    //if there is an error, console log the error, other wise console log commit logged!
+    err ? console.error(err) : console.log('Commit logged!')
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile('./Created-File/README.md', 'test')
+}
 
 // Function call to initialize app
 init();
