3def9a9b6e978f87a39055f0c5689f5df0bfb2bb
unknown <blancardt@merrimack.edu>
2023-08-25 12:03:54 -0400

    changed the 4th question to give a list of options to choose from instead of inputtting an answer and added the result to the README file.

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const fs = require('fs');
 
 const questions = ['What is your GitHub username?', 'What is your email address?', 'What is your project\'s name?', 'Please writ a short description of your project?', 'What kind of license does your project have?', 'What command should be run to install dependencies?', 'What command should be run to run tests?', 'What does the user need to know about using the repo?', 'What does the user need to know about contributing to the repo?'];
 
+//Went to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll to figure out how to replace spaces with underscores.
+//also credited in the README file.
 //function that creates the text for the README file.
-const createText = ({ userName, email, title, description }) =>
+const createText = ({ userName, email, title, description, license }) =>
   `# ${title}  
+![License: ${license}](https://img.shields.io/badge/License-${license.replaceAll(' ', '_')}-blue.svg)    
+
 ## Description  
 ${description}  
+
 ## Questions  
 If you have any questions about the repo, open an issue or contact me directly at ${email}.  You can find more of my work at [${userName}](https://github.com/${userName}/).  `;
 
@@ -45,8 +50,9 @@ function init() {
       name: 'description',
     },
     {
-      type: 'input',
+      type: 'list',
       message: questions[4],
+      choices: [ "MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None" ],
       name: 'license',
     },
   ])
